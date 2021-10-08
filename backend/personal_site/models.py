685ff8f5a90c75d95d88f92d7e4563435da84a6f
karimabuseer <karimabuseer@gmail.com>
2021-10-08 15:54:59 +0100

    Created initial backend model for project

--- backend/personal_site/models.py
+++ backend/personal_site/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    image_link =  models.TextField()
+    demo_link = models.Textfield()
+    
