cf488a4a0bf47b5e9bd2403758bfac4ba43ae3aa
karimabuseer <karimabuseer@gmail.com>
2021-10-11 18:00:48 +0100

    Required project model in admin

--- backend/personal_site/admin.py
+++ backend/personal_site/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
 class PersonalSiteAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'image_link', 'demo_link')
+# Register your models here.
+
+admin.site.register(Project, PersonalSiteAdmin)
