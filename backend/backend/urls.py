b8e7526a2e1cb1dae445daccfdabe2873baafd4f
karimabuseer <karimabuseer@gmail.com>
2021-10-12 10:32:57 +0100

    Created CRUD api for project

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from personal_site import views
+
+router = routers.DefaultRouter()
+router.register(r'projects', views.ProjectView, 'project')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
