e8c474f6211eb9b9d78da658b5de02eec1fde5eb
karimabuseer <karimabuseer@gmail.com>
2021-10-12 10:22:15 +0100

    Added project views

--- backend/personal_site/views.py
+++ backend/personal_site/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProjectSerializer
+from .models import Project
 
 # Create your views here.
+class ProjectView(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
+    
