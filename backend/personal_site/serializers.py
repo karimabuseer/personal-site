81b8a3524f29867d32c0bdc6079cab1f00055828
karimabuseer <karimabuseer@gmail.com>
2021-10-19 18:06:04 +0100

    Finished updating project serializer

--- backend/personal_site/serializers.py
+++ backend/personal_site/serializers.py
@@ -4,4 +4,6 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('id', 'title', 'order', 'description', 'image_link', 'demo_link')
+        fields = ('id', 'title', 'order', 'short_description', 
+        'long_description', 'image_one_link', 'image_two_link',
+        'image_three_link','demo_link')
